97ff522c8c482e72fc5b5fd11705d8aaeb23e290
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-12-14 15:22:17 +0200

    Initial commit

---
+++ Lab10.2.cpp
@@ -0,0 +1,38 @@
+﻿/*
+Дано текстовий файл t.
+Групи  символів,  відокремлені  пробілами  (одним  або  кількома)  і  які  не  містять пробілів, будемо називати словами.
+Знайти останнє слово, яке починається з букви “a”.
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+string ProcessTXT(char* fname) {
+    ifstream fin(fname);
+    string s;
+    string w;
+    int k = 0;
+    while (fin >> s)
+    {
+        if (s[0] == 'a') {
+            w = s;
+        }
+    }
+    return w;
+}
+
+
+
+
+int main()
+{
+    char name[6] = "t.txt";
+    cout << "Last word start of 'a': " << ProcessTXT(name) << endl;
+
+    cin.get();
+    return 0;
+}
+
---
+++ UnitTest10.2/UnitTest10.2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab10.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest102
+{
+	TEST_CLASS(UnitTest102)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string s = "aaaaAAA";
+			string t = ProcessTXT("../t.txt");
+			Assert::AreEqual(s, t);
+		}
+	};
+}
